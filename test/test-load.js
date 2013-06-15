13c7edb966b3f9bcb8c32c66fc11af9fc983cd33
Freyskeyd <contact@freyskeyd.fr>
2013-06-15 19:14:36 +0200

    Test fail travis

--- test/test-load.js
+++ test/test-load.js
@@ -6,6 +6,6 @@ var assert  = require('assert');
 describe('laravel generator', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
-    assert(app !== undefined);
+    assert(app === undefined);
   });
 });
